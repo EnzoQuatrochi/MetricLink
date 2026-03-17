6b5f38f7dfe0e1021c608241ae1f8c51c0b6da11
Enzo <enzoquatrochi@outlook.com>
2026-03-17 18:32:32 -0300

    Fix boolean tests

--- tests/test_url.py
+++ tests/test_url.py
@@ -5,11 +5,11 @@ def test_valid_url():
     
     expires_at = (datetime.now() + timedelta(days=1)).isoformat()
     url = Url(slug="test1", original_url="https://leetcode.com/u/enzoquatrochi/", expires_at = expires_at)
-    assert url.is_expired() == False
+    assert not url.is_expired() 
 
-def test_invalid_url():
+def test_exipired_url():
     
     expires_at = (datetime.now() - timedelta(days=1)).isoformat()
     url = Url(slug="test1", original_url="https://leetcode.com/u/enzoquatrochi/", expires_at = expires_at)
-    assert url.is_expired() == True
+    assert url.is_expired() 
     
