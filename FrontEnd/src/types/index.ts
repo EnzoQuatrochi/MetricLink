2c082d681a8e51b34fcacfb20fd1f7aef21878c6
Enzo <enzoquatrochi@outlook.com>
2026-05-08 02:07:46 -0300

    feat: create react project structure and implement api service

---
+++ FrontEnd/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Url {
+    slug: string
+    original_url: string
+    created_at: string
+    expires_at: string
+}
+  
+export interface Metrics {
+    total_clicks: number
+    clicks_per_day: number
+}
