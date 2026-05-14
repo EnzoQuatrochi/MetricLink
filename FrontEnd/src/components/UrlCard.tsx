9838fdbf1c1a0b04286bddf80a2be0fec96b4ada
Enzo <enzoquatrochi@outlook.com>
2026-05-13 22:12:19 -0300

    feat: implement cors and multiples screens

--- FrontEnd/src/components/UrlCard.tsx
+++ FrontEnd/src/components/UrlCard.tsx
@@ -3,10 +3,11 @@ import type { Url } from '../types'
 import './UrlCard.css'
 
 interface UrlCardProps {
-    url: Url
+  url: Url
+  onBack: () => void
 }
 
-export default function UrlCard({ url }: UrlCardProps){
+export default function UrlCard({ url, onBack }: UrlCardProps){
 
     const navigate = useNavigate()
 
@@ -15,15 +16,22 @@ export default function UrlCard({ url }: UrlCardProps){
     }
 
     return(
-        <div className="form">
-            <h1>Informações da Url</h1>
-            <p><strong>Url Curta: </strong>http://127.0.0.1:8000/{url.slug}</p>
-            <p><strong>Url original: </strong>{url.original_url}</p>
-            <p><strong>Criada em: </strong>{new Date(url.created_at).toLocaleDateString('pt-BR')}</p>
-            <p><strong>Expira em: </strong>{new Date(url.expires_at).toLocaleDateString('pt-BR')}</p>
-            <div className="url-card-buttons">
-                <button className="copyButton" onClick={copyToClipboard}>Copiar URL</button>
-                <button className="submitButton" onClick={() => navigate(`/metrics/${url.slug}`)}>Ver métricas</button>
+        <div className='urlCard'>
+            <div className='topButton'>
+                <button className="backButton" onClick={onBack}>🠔 Voltar</button>
+            </div>
+            <div className='formPosition'>
+                <div className="card_form">
+                    <h1>Informações da Url</h1>
+                    <p><strong>Url Curta: </strong>http://127.0.0.1:8000/{url.slug}</p>
+                    <p><strong>Url original: </strong>{url.original_url}</p>
+                    <p><strong>Criada em: </strong>{new Date(url.created_at).toLocaleDateString('pt-BR')}</p>
+                    <p><strong>Expira em: </strong>{new Date(url.expires_at).toLocaleDateString('pt-BR')}</p>
+                    <div className="url-card-buttons">
+                        <button className="copyButton" onClick={copyToClipboard}>Copiar URL</button>
+                        <button className="submitButton" onClick={() => navigate(`/metrics/${url.slug}`)}>Ver métricas</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
